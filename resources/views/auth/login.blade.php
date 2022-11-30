394e399ddc70963e99bc817bd86a83a72cdd448d
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2022-11-30 07:47:19 +0700

    fix login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,30 +29,30 @@
         @csrf
         <div class="input-group mb-3">
           <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-          @error('email')
-            <span class="invalid-feedback" role="alert">
-                 <strong>{{ $message }}</strong>
-            </span>
-          @enderror
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
             </div>
           </div>
         </div>
+        @error('email')
+        <span class="invalid-feedback" role="alert">
+             <strong>{{ $message }}</strong>
+        </span>
+      @enderror
         <div class="input-group mb-3">
           <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password" required autocomplete="current-password">
-           @error('password')
-             <span class="invalid-feedback" role="alert">
-                   <strong>{{ $message }}</strong>
-             </span>
-           @enderror       
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
             </div>
           </div>
         </div>
+        @error('password')
+             <span class="invalid-feedback" role="alert">
+                   <strong>{{ $message }}</strong>
+             </span>
+           @enderror   
         <div class="row">
           <div class="col-8">
             <div class="icheck-primary">
