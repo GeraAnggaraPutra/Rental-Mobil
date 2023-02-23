56cf986730e1388d2e70c2783e94423b0554c651
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-02-23 09:48:46 +0700

    fix register

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -24,7 +24,6 @@ class User extends Authenticatable implements MustVerifyEmail
         'role',
         'id_google',
         'id_facebook',
-        'email_verified_at',
     ];
 
     /**
